b7d4eaf64b3b1d3c705d8896b750ccb69a5dad2e
iravasen <ivan.ravasenga@cern.ch>
2019-02-06 12:27:59 +0100

    bug stave - solved again

--- utils/utils.C
+++ utils/utils.C
@@ -79,6 +79,7 @@ string ReadHSID(string fpath){
 
 //Extract Stave id from file path
 string ReadStaveID(string fpath){
+  if(fpath.find("OBHIC") == string::npos) return "-------------";
   string staveid = fpath.substr(fpath.find("OBHIC")-29, 14);
   return staveid;
 }
