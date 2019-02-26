c2f17b029b445c7c0a529733f86892f934b1e529
iravasen <ivan.ravasenga@cern.ch>
2019-02-26 12:27:56 +0100

    T-OL-Stave-012 rec test added manually for the moment

--- stave-rec-monitoring.C
+++ stave-rec-monitoring.C
@@ -18,11 +18,12 @@ bool staverecmonitoring(){
   ofstream outfl("staveresults_rec.dat");
 
   //Black list: HSs that are in the DB but have some problems: missing HIC QT, missing attachments (added manually below)
-  string blacklist = "F-OL-Stave-008";
+  string blacklist = "F-OL-Stave-008, T-OL-Stave-012";
   //Add old HS, HS without attachments by hand (from excel)
-  const int nManual = 1;
+  const int nManual = 2;
   string shsmanual[nManual] = {
     "F-OL-Stave-008 98 98 30/11/2018 48"
+    "T-OL-Stave-012 98 97 25/2/2019 9"
     };
   for(int i=0; i<nManual; i++)
     outfl<<shsmanual[i]<<endl;
