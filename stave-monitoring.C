3796032d0019af4beb369fec398ed60ea6817c50
iravasen <ivan.ravasenga@cern.ch>
2019-09-06 15:57:27 +0200

    update for ML: 2 staves (temporarily)

--- stave-monitoring.C
+++ stave-monitoring.C
@@ -13,7 +13,7 @@ bool stavemonitoring(){
   string blacklist = "T-OL-Stave-003, R-OL-Stave-002, R-OL-Stave-004, B-ML-Stave-039, T-OL-Stave-002, D-OL-Stave-008, T-OL-Stave-006, B-ML-Stave-011, A-OL-Stave-001, A-OL-Stave-002, A-OL-Stave-003, A-OL-Stave-007, A-OL-Stave-011, F-OL-Stave-001";
 
   //Add old HS, HS without attachments by hand (from excel)
-  const int nManual = 10;
+  const int nManual = 12;
   string shsmanual[nManual] = {
     //"D-OL-Stave-008 D-OL-HS-U-008 D-OL-HS-L-008 98 84 17/5/2019 20",
     "B-ML-Stave-011 B-ML-HS-U-011 B-ML-HS-L-011 56 56 12/11/2018 46",
@@ -27,7 +27,9 @@ bool stavemonitoring(){
     //"F-OL-Stave-001 F-OL-HS-U-001 F-OL-HS-L-001 55 84 14/9/2018 37",
     "T-OL-Stave-006 T-OL-HS-U-006 T-OL-HS-L-006 98 98 23/7/2018 30",
     "T-OL-Stave-003 T-OL-HS-U-003 T-OL-HS-L-003 92 96 11/7/2018 28",
-    "R-OL-Stave-004 A-OL-HS-U-003 A-OL-HS-L-103 98 98 19/7/2019 29"
+    "R-OL-Stave-004 A-OL-HS-U-003 A-OL-HS-L-103 98 98 19/7/2019 29",
+    "B-ML-Stave-061 B-ML-HS-U-061 B-ML-HS-L-061 56 56 5/9/2019 36",
+    "B-ML-Stave-062 B-ML-HS-U-062 B-ML-HS-L-062 56 56 6/9/2019 36"
   };
 
   for(int i=0; i<nManual; i++)
